a928ed6ca829495d0a7231516f1646849c78c34d
Ashwin Balasubramanian <56084309+sinking8@users.noreply.github.com>
2022-06-07 21:07:42 +0530

    Update train.py

--- train.py
+++ train.py
@@ -19,41 +19,41 @@ def train_fn(loader, disc, gen, opt_gen, opt_disc, mse, bce, vgg_loss):
     # print(loop[0])
     inx = 0
 
-    for idx, (low_res, high_res) in enumerate(loop):
+#    for idx, (low_res, high_res) in enumerate(loop):
         # high_res = high_res.to(config.DEVICE)
         # low_res = low_res.to(config.DEVICE)
         
         ### Train Discriminator: max log(D(x)) + log(1 - D(G(z)))
-        fake = gen(low_res)
-        disc_real = disc(high_res)
-        disc_fake = disc(fake.detach())
-        disc_loss_real = bce(
-            disc_real, torch.ones_like(disc_real) - 0.1 * torch.rand_like(disc_real)
-        )
-        disc_loss_fake = bce(disc_fake, torch.zeros_like(disc_fake))
-        loss_disc = disc_loss_fake + disc_loss_real
+#        fake = gen(low_res)
+#        disc_real = disc(high_res)
+#        disc_fake = disc(fake.detach())
+#        disc_loss_real = bce(
+#            disc_real, torch.ones_like(disc_real) - 0.1 * torch.rand_like(disc_real)
+#        )
+#        disc_loss_fake = bce(disc_fake, torch.zeros_like(disc_fake))
+#        loss_disc = disc_loss_fake + disc_loss_real
 
-        opt_disc.zero_grad()
-        loss_disc.backward()
-        opt_disc.step()
+#        opt_disc.zero_grad()
+#        loss_disc.backward()
+#        opt_disc.step()
 
         # Train Generator: min log(1 - D(G(z))) <-> max log(D(G(z))
-        disc_fake = disc(fake)
+#        disc_fake = disc(fake)
         #l2_loss = mse(fake, high_res)
-        adversarial_loss = 1e-3 * bce(disc_fake, torch.ones_like(disc_fake))
+#        adversarial_loss = 1e-3 * bce(disc_fake, torch.ones_like(disc_fake))
         #loss_for_vgg = 0.006 * vgg_loss(fake, high_res)
         #gen_loss = loss_for_vgg + adversarial_loss
-        coeffs = pywt.dwt2(low_res, 'haar')
-        cA, (cH, cV, cD) = coeffs
+#        coeffs = pywt.dwt2(low_res, 'haar')
+#        cA, (cH, cV, cD) = coeffs
 
 
-        gen_loss = adversarial_loss
-        opt_gen.zero_grad()
-        gen_loss.backward()
-        opt_gen.step()
+#        gen_loss = adversarial_loss
+#        opt_gen.zero_grad()
+#        gen_loss.backward()
+#        opt_gen.step()
 
-        if idx % 200 == 0:
-            plot_examples("test_images/yes", gen)
+#        if idx % 200 == 0:
+         plot_examples("test_images/yes", gen)
 
 
 def main():
